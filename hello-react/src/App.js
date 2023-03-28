99e6cb8db0c8f366de55f88e10b2edfe49167418
lsw02 <sw020726@naver.com>
2023-03-28 16:08:33 +0900

    React Study

---
+++ hello-react/src/App.js
@@ -0,0 +1,20 @@
+import React, {Componet} from "react";
+import "./App.css";
+import MyComponent from "./MyComponent";
+import Say from "./Say";
+import EventPractice from "./EventPractice";
+import ValidationSample from "./ValidationSample";
+
+class App extends Componet {
+  render() {
+    return
+  }
+}
+
+// const App = () => {
+//   return(
+//   <EventPractice />
+//   );
+// };
+
+export default App;
---
+++ hello-react/src/MyComponent.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const MyComponent = ({name, children, favoriteNumber}) => {
+    return (    
+    <div>
+    안녕하세요, 제 이름은 {name}입니다.
+    children = {children}
+    <br />
+    좋아하는 숫자 = {favoriteNumber}
+    </div>
+    );
+};
+
+ MyComponent.defaultProps = {
+    name: '기본 이름'
+ };
+
+ MyComponent.propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired
+ };
+export default MyComponent;
