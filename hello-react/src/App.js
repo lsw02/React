2679d17147e4bdf3d52b1f87febe5fa89c66498c
lsw02 <sw020726@naver.com>
2023-03-31 20:06:45 +0900

    Chapter6

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,13 +1,18 @@
-import React, {Componet} from "react";
+import React, {Component} from "react";
 import "./App.css";
 import MyComponent from "./MyComponent";
 import Say from "./Say";
 import EventPractice from "./EventPractice";
 import ValidationSample from "./ValidationSample";
+import IterationSample from "./IterationSample";
 
-class App extends Componet {
+class App extends Component {
   render() {
-    return
+    return (
+      <div>
+        <IterationSample />
+      </div>
+    );
   }
 }
 
