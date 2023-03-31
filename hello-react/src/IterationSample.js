6a012bf1d7281a673606d0c686c1fe102b99c788
lsw02 <sw020726@naver.com>
2023-03-31 21:19:53 +0900

    Chapter6

--- hello-react/src/IterationSample.js
+++ hello-react/src/IterationSample.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const IterationSample = () => {
+    const [name, SetName] = useState([
+        {id: 1, text: '눈사람'},
+        {id: 2, text: '얼음'},
+        {id: 3, text: '눈'},
+        {id: 4, text: '바람'}
+    ]);
+    const [inputText, setInputText] = useState('');
+    const [nextId, setNextId] = useState(5);
+
+    const onChange = e => setInputText(e.target.value);
+    const onClick = () => {
+        const nextName = name.concat({
+            id: nextId,
+            text: inputText
+        });
+        setNextId(nextId+1);
+        SetName(nextName);
+        setInputText('');
+    };
+
+    const nameList = name.map(name => <li key={name.id}>{name.text}</li>);
     return(
-        <ul>
-            <li>눈사람</li>
-            <li>얼음</li>
-            <li>눈</li>
-            <li>바람람</li>
-        </ul>
+        <div>
+            <input value={inputText} onChange={onChange} />
+            <button onClick={onClick}>추가</button>
+            <ul>{nameList}</ul>
+        </div>
     );
-};
+}
+
 
 export default IterationSample;
