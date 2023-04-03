17eedc7f6ab678f66174193aebd50f4f5ed1d711
lsw02 <sw020726@naver.com>
2023-04-03 22:47:39 +0900

    Chapter8.2

--- hooks-tutorial/src/App.js
+++ hooks-tutorial/src/App.js
@@ -1,12 +1,21 @@
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Counter from './Counter';
 import Info from './Info'
 
 const App = () => {
+  const [visible, setVisible] = useState(false)
   return(
     <div>
-      <Info />
+      <button onClick={() => {
+        setVisible(!visible)
+      }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
     </div>
   );
 }
